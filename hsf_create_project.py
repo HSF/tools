68f84273f10018071004458bbde215d524cf4e64
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-04-25 16:05:41 +0100

    Create/use uninstall target
    By default, CMake does not provide an `uninstall` target. Use KDE
    example implementation for the template project to provide this
    useful target out the box.

--- hsf_create_project.py
+++ hsf_create_project.py
@@ -104,6 +104,8 @@ class ProjectCreator(object):
         os.rename(join(self.target_dir,"cmake/HSFTEMPLATEConfig.cmake.in"),join(self.target_dir,"cmake/%sConfig.cmake.in" %self.name))
         os.rename(join(self.target_dir,"cmake/HSFTEMPLATECreateConfig.cmake"),join(self.target_dir,"cmake/%sCreateConfig.cmake" %self.name))
         os.rename(join(self.target_dir,"cmake/HSFTEMPLATEDoxygen.cmake"),join(self.target_dir,"cmake/%sDoxygen.cmake" %self.name))
+        os.rename(join(self.target_dir,"cmake/HSFTEMPLATEUninstall.cmake"),join(self.target_dir,"cmake/%sUninstall.cmake" %self.name))
+        os.rename(join(self.target_dir,"cmake/HSFTEMPLATE_uninstall.cmake.in"),join(self.target_dir,"cmake/%s_uninstall.cmake.in" %self.name))
         os.rename(join(self.target_dir,"HSFTEMPLATEVersion.h"),join(self.target_dir,"%sVersion.h" %self.name))
         os.rename(join(self.target_dir,"package/include/example"),join(self.target_dir,"package/include/%s" %self.name))
         os.rename(join(self.target_dir,"package"),join(self.target_dir,"%s" %self.subpackage_name))
